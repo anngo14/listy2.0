786977cad615a12e83e49fc6ac1e6d76ccfc6437
Andrew <aandrewnngo@gmail.com>
2020-12-30 10:52:30 -0600

    added to the about page

--- src/components/MainContent/About/About.js
+++ src/components/MainContent/About/About.js
@@ -10,7 +10,7 @@ export default function About() {
                     Listy is a To Do List application that was built using React. As a common starter project for most budding developers, Listy takes the concept of a To-Do List and is reimagined with my own image. 
                 </p>
                 <p>
-                    Most web applications with simple functionality are not going to be visited often or for long periods of time. Taking this into account I wanted to provide the user a really quick glimpses of information that would be useful. For example, the header on the Home page was meant to grab the user's attention quickly to tell the user what general time of day it is by using a duller color for other elements on the page.
+                    Most web applications with simple functionality are not going to be visited often or for long periods of time. Taking this into account I wanted to provide the user a really quick glimpses of information that would be useful. For example, the header on the Home page was meant to grab the user's attention quickly to tell the user what general time of day it is by using a duller color for other elements on the page. I'm terrible at drawing so the vision of the header never truly came into fruition, however this was the idea behind the header component on the Home page. 
                 </p>
             </div>
         </div>
