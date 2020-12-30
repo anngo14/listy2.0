c2f46df28e47bcdb55dca682a2e5026cdecbc90b
Andrew <aandrewnngo@gmail.com>
2020-12-30 10:50:05 -0600

    added header assets and started animations for morning

--- src/components/MainContent/Home/Header.js
+++ src/components/MainContent/Home/Header.js
@@ -1,17 +1,53 @@
 import React, { Component } from 'react'
 import { Card } from 'react-bootstrap'
 import '../../../css/MainContent/Home/Header.css'
+import Cloud from './assets/Morning/Cloud.png'
+import MCloud from './assets/Morning/Multiple Clouds.png'
+import Sun from './assets/Morning/Sun.png'
+import Moon from './assets/Night/Moon.png'
+import WStar from './assets/Night/White star.png'
+import YStar from './assets/Night/Yellow star.png'
 
 export default class Header extends Component {
     months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
     state = {
         date: new Date(),
-        planModal: false
+        planModal: false,
+        headerContent: null,
+        headerStyle: {}
     }
 
     componentDidMount(){
         this.clockInterval = setInterval(() => {
             this.tick();
+            if(this.state.date.getHours() >= 5 && this.state.date.getHours() <= 19){
+                this.setState({
+                    headerStyle: {
+                        backgroundColor: '#87CEEB'
+                    },
+                    headerContent: (
+                        <div className='header-content'>
+                            <img alt='cloud' src={Cloud} className='clouds' id='cloud1' />
+                            <img alt='cloud' src={Cloud} className='clouds' id='cloud2' />
+                            <img alt='clouds' src={MCloud} id='clouds' />
+                            <img alt='sun' src={Sun} id='sun' />
+                        </div>
+                    )
+                })
+            } else{
+                this.setState({
+                    headerStyle: {
+                        backgroundColor: '#0C1445'
+                    },
+                    headerContent: (
+                        <div className='header-content'>
+                            <img alt='moon' src={Moon} id='moon' />
+                            <img alt='white-star' src={WStar} className='star' />
+                            <img alt='yellow-star' src={YStar} className='star' />
+                        </div>
+                    )
+                })
+            }
         }, 1000);
     }
     componentWillUnmount(){
@@ -30,8 +66,9 @@ export default class Header extends Component {
                     <h1>{this.state.date.getDate()}</h1>
                     <span>{this.state.date.getFullYear()}</span>
                 </div>
-                <div className='home-header-content'>
+                <div className='home-header-content' style={this.state.headerStyle}>
                     <span id='home-header-time'>{this.state.date.toLocaleTimeString()}</span>
+                    {this.state.headerContent}
                 </div>
             </Card>
         )
